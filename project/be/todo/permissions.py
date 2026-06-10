aa7999e4962e444a258b2542981821120343b4d4
Aniello Piscopo <aniello.piscopo0707@gmail.com>
2026-06-10 21:35:15 +0200

    Managed multi user permissions, create , edit , delete and put of tasks

---
+++ project/be/todo/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from .models import Todo
+
+__all__ = ['IsAuthorOrReadOnly']
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request: Request, view: APIView, obj: Todo) -> bool:
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return obj.todo_of == request.user
--- project/be/todo/views/api.py
+++ project/be/todo/views/api.py
@@ -1,13 +1,21 @@
+from django.db.models import QuerySet
 from rest_framework import generics
 from ..models import Todo
 from ..serializers import TodoSerializer
+from ..permissions import IsAuthorOrReadOnly
 
 __all__ = ['TodoDetailView', 'TodoListView']
 
-class TodoListView(generics.ListAPIView):
+class TodoListView(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
-    queryset = Todo.objects.all().order_by('is_completed', 'set_to_complete', 'goal_set_date')
 
-class TodoDetailView(generics.RetrieveAPIView):
+    def get_queryset(self) -> QuerySet:
+        return Todo.objects.all().order_by('is_completed', 'set_to_complete', 'goal_set_date')
+
+    def perform_create(self, serializer) -> None:
+        serializer.save(todo_of=self.request.user)
+
+class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+    permission_classes = [IsAuthorOrReadOnly]
