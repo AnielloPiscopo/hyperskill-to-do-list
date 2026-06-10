85777f75b078a49619ada15ce431a64cf7148506
Aniello Piscopo <aniello.piscopo0707@gmail.com>
2026-06-10 09:19:09 +0200

    Modified settings.py

--- project/be/django_to_do_list/settings.py
+++ project/be/django_to_do_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*im$drs387rs^6v95q(#tf_!z8ms1qn*s1rsrxap4mz)uzgk8='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
