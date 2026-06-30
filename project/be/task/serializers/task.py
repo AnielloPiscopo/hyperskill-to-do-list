060dfe6bdb9a7806d1b6a16d41240644b50776ad
Aniello Piscopo <aniello.piscopo0707@gmail.com>
2026-06-30 10:46:17 +0200

    Made a fix

--- project/be/task/serializers/task.py
+++ project/be/task/serializers/task.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
+from core.serializers import BaseModelSerializer
 from task.models import Task
 
 __all__ = ['TaskSerializer']
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class TaskSerializer(BaseModelSerializer):
     class Meta:
         model = Task
         exclude = ['user', 'is_archived']
+
+    def validate_board(self, board):
+        if board is None:
+            return board
+        request = self.context.get('request')
+        if board.user != request.user:
+            raise serializers.ValidationError('This board does not belong to you.')
+        return board
