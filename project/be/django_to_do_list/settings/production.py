36ae8a1f698c87ff1da95d908301e0aeb8db6440
Aniello Piscopo <aniello.piscopo0707@gmail.com>
2026-08-04 10:34:06 +0200

    Commit

--- project/be/django_to_do_list/settings/production.py
+++ project/be/django_to_do_list/settings/production.py
@@ -1,5 +1,17 @@
+import os
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['yourdomain.com']
-DATABASES = {...}
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
