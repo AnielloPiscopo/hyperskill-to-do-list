db023f81644bcfe2f4c7335310dacd9e09776734
Aniello Piscopo <aniello.piscopo0707@gmail.com>
2026-08-28 09:39:45 +0200

    Fix urls

--- project/be/task/urls.py
+++ project/be/task/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
     path('<int:pk>/', TaskDetailView.as_view()),
     path('archive-all/', TaskArchiveAllView.as_view()),
     path('restore-all/', TaskRestoreAllView.as_view()),
+    path('delete-all/', TaskDestroyAllView.as_view()),
+    path('move/', TaskMoveView.as_view()),
     path('<int:pk>/archive/', TaskArchiveView.as_view()),
     path('<int:pk>/restore/', TaskRestoreView.as_view()),
-    path('move/', TaskMoveView.as_view()),
-    path('delete-all/', TaskDestroyAllView.as_view()),
 ]
